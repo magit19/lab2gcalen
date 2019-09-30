c37f19d820475e5291dfb6a27b0aaac4ec928548
Neustroev <ea.neustroev@stud.s-vfu.ru>
2019-09-30 17:24:34 +0900

    печатать пары из excel

---
+++ xlrasp.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+import xlrd
+book = xlrd.open_workbook('imi2019.xls')
+sh = book.sheets()[9]
+n = 0
+for r in range(3,39):
+    para = sh.cell(r, 23).value
+    room = sh.cell(r, 25).value
+    if para != "" :
+        para = para.replace("\n"," ")
+        print(para, room)
+        n = n + 1
+print(n)
