983ea07665690e0a0ebfeeecf1803fe8347c3daa
NikiforovB <egor19970307@gmail.com>
2019-10-07 16:16:38 +0900

    testovy commit

--- quickstart.py
+++ quickstart.py
@@ -8,7 +8,7 @@ from oauth2client import file, client, tools
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 
 def main():
-    """Shows basic usage of the Google Calendar API.
+    """Shows basic usage of the Google Calendar API.....
     Prints the start and name of the next 10 events on the user's calendar.
     """
     store = file.Storage('token.json')
