72dc0d5518334c83fca6b08acbe275d85ef9e8c8
potapov <mega.sentinel@gmail.com>
2019-10-10 19:47:09 +0900

    sdelal

--- quickstart.py
+++ quickstart.py
@@ -67,6 +67,22 @@ def main():
 
 
     #add event
+    import xlrd                                                   
+                                                              
+	start = ['08:00', '09:50', '11:40', '14:00', '15:50', '17:40',]
+	end = ['09:35', '11:25', '13:15', '15:35', '17:25', '19:15',]  
+	                                                              
+	book = xlrd.open_workbook('imi2019.xls')                      
+	mag = book.sheet_by_index(9)                                  
+	                                                              
+	for i in range(3,39):                                         
+	    if mag.cell(i, 20).value == "":                            
+	        continue                                              
+	    para = mag.cell(i, 20).value                               
+	    l_pr = mag.cell(i, 21).value                               
+	    room = mag.cell(i, 22).value    
+	    day = 2 + (i-3)//6                          
+	    print(day, "сен", start[(i-3)%6], '-', end[(i-3)%6],    para, l_pr, room)  
 
     event = {
       'summary': 'Психология лидерства Нафанаилова М.С.',
